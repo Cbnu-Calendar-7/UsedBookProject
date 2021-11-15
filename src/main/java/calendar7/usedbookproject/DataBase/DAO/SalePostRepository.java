553c0beb1055298c9d21a9a4f3bbc9a4b631f157
HseongN <byong1121@naver.com>
2021-11-15 18:08:07 +0900

    Spring JPA Change CrudRepository -> JpaRepository

--- src/main/java/calendar7/usedbookproject/DataBase/DAO/SalePostRepository.java
+++ src/main/java/calendar7/usedbookproject/DataBase/DAO/SalePostRepository.java
@@ -2,8 +2,6 @@ package calendar7.usedbookproject.DataBase.DAO;
 
 
 import calendar7.usedbookproject.DataBase.DTO.SalePost;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SalePostRepository extends CrudRepository<SalePost, Long>
-{
-}
+public interface SalePostRepository extends JpaRepository<SalePost, Long> { }
--- src/main/java/calendar7/usedbookproject/DataBase/DAO/UserRepository.java
+++ src/main/java/calendar7/usedbookproject/DataBase/DAO/UserRepository.java
@@ -1,9 +1,6 @@
 package calendar7.usedbookproject.DataBase.DAO;
 
-
 import calendar7.usedbookproject.DataBase.DTO.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends CrudRepository<User, String>
-{
-}
+public interface UserRepository extends JpaRepository<User, String> { }
