eb9afc96daf47e3b76955012d878671f75f57487
HseongN <byong1121@naver.com>
2021-11-27 02:01:42 +0900

    Commit

--- src/main/java/calendar7/usedbookproject/Controller/ProfileController.java
+++ src/main/java/calendar7/usedbookproject/Controller/ProfileController.java
@@ -1,19 +1,28 @@
 package calendar7.usedbookproject.Controller;
 
+import calendar7.usedbookproject.DataBase.DTO.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProfileController
 {
 
-    @GetMapping(path = "profile/mylist")
-    public String MyList()
+    @GetMapping(path = "mylist")
+    public String MyList(Model model)
     {
+        User user = new User("TestID", "1234", "test@email.com", "01012345678", "TestNick", "TestName");
+
+        String list = "1,2,3,4,5,6";
+        user.setSaleslist(list);
+        user.setWishlist(list);
+
+        model.addAttribute("user", user);
         return "profile/mylist";
     }
 
-    @GetMapping(path = "profile/archive")
+    @GetMapping(path = "archive")
     public String Archive()
     {
         return "profile/archive";
